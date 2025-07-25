3f5f85b25205f3598f67776a1d3420f0f41671bf
Sreepathy <sreepathy200567@gmail.com>
2025-07-25 20:36:01 +0530

    reinstalled vite and added Home in pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Landing from "./pages/Landing";
+import Home from "./pages/home/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Root route renders Landing page */}
-        <Route path="/" element={<Landing />} />
+        {/* <Route path="/" element={<Landing />} /> */}
+        <Route path="/" element={<Home/>}/>
+        {/* <Route path="/login" element={<Login/>}/>
+        <Route path="/dashboard" element={<DashBoard/>}></Route> */}
         {/* Add more routes here as your app grows */}
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+// import NavBar from "../../components/navbar/navBar";
+// import ImageHeader from "../../components/ImageHeader/imageHeader";
+// import Features from "../../components/Features/Features";
+// import Steps from "../../components/HowItWorks/Steps";
+// import AboutUs from "../../components/AboutUs/AboutUs";
+const Home = ()=>{
+    return (
+        <div className="home">
+            hi
+            {/* <NavBar/>
+            
+            <ImageHeader/>
+            <Features/>
+            <Steps/> */}
+        </div>
+    );
+}
+
+export default Home
+
